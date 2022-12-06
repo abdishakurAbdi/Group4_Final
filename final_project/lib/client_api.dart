2b137b08690439a31cb14c07b6e72a82655f44ea
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2022-12-05 21:52:55 -0600

    created code for api access

---
+++ final_project/lib/client_api.dart
@@ -0,0 +1,54 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class ClientApi {
+  static const String _url = "http://10.0.2.2:1200";
+  final Dio _dio = Dio(BaseOptions(baseUrl: _url));
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+
+  Future login(String employeeId, String password) async {
+    final response = await _dio
+        .post('/login', data: {'employeeID': employeeId, 'password': password});
+
+    await _storage.write(
+        key: 'accessToken', value: response.data['accessToken']);
+  }
+
+  Future addNewCar(int year, String make, String model, String price) async {
+    final accessToken = await _storage.read(key: 'accessToken');
+    _dio.options.headers['authorization'] = "Bearer $accessToken";
+
+    final response = await _dio.post('/addNewCar',
+        data: {'year': year, 'make': make, 'model': model, 'price': price});
+  }
+
+  Future addUsedCar(
+      int year, String make, String model, String miles, String price) async {
+    final accessToken = await _storage.read(key: 'accessToken');
+    _dio.options.headers['authorization'] = "Bearer $accessToken";
+
+    await _dio.post('/addUsedCar', data: {
+      'year': year,
+      'make': make,
+      'model': model,
+      'miles': miles,
+      'price': price
+    });
+  }
+
+  Future logout() async {
+    await _storage.delete(key: 'accessToken');
+  }
+
+  Future<List> getNewCars() async {
+    final response = await _dio.get('/getNewCars');
+
+    return response.data['newCars'];
+  }
+
+  Future<List> getUsedCars() async {
+    final response = await _dio.get('/getUsedCars');
+
+    return response.data['usedCars'];
+  }
+}
