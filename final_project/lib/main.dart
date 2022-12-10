028ac774522a597de28904f32f123543ff2e71a0
abdishakurAbdi <74845430+abdishakurAbdi@users.noreply.github.com>
2022-12-09 23:24:19 -0600

    background pic working

--- final_project/lib/main.dart
+++ final_project/lib/main.dart
@@ -29,41 +29,28 @@ class RootPage extends StatefulWidget {
  
   class _RootPageState extends State<RootPage> {
     int currentPage = 0;
-    @override 
-    Widget build(BuildContext context) {
-      return Container(
+    @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("widget.title"),
+      ),
+      body: Container(
         constraints: const BoxConstraints.expand(),
         decoration: const BoxDecoration(
-          image: DecorationImage (
-            image: AssetImage ('images\Group4Logo.png'),
-            fit: BoxFit.cover),),
-            child: Scaffold(
-          
-        
-            appBar: AppBar(
-              title: const Text('Flutter'),
-            ),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () {
-              debugPrint('Floating Action Button');
-            },
-            child: const Icon(Icons.home),
-            ),
-            bottomNavigationBar: NavigationBar(
+          image: DecorationImage(
+              image: AssetImage("images/Group4Logo.png"),
+          fit: BoxFit.cover,
+        )
+      ),
+    ),
+    bottomNavigationBar: NavigationBar(
               destinations: const [
                 NavigationDestination(icon: Icon(Icons.login), label: 'Employee Login'),
                 NavigationDestination(icon: Icon(Icons.car_rental), label: 'Inventory'),
                 NavigationDestination(icon: Icon(Icons.document_scanner), label: 'Contact Us'),
                 
               ],
-              onDestinationSelected: (int index) {
-                setState(() {
-                  currentPage = index;
-                });
-              },
-              selectedIndex: currentPage,
-            )
-          )
-        );
-      }
+    ));
+  }
 }
