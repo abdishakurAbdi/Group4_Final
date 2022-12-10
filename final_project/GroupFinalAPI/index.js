c3e02298343caf3a7be81e52d1e07f9e3ed57595
abdishakurAbdi <74845430+abdishakurAbdi@users.noreply.github.com>
2022-12-09 22:02:23 -0600

    Changes

--- final_project/GroupFinalAPI/index.js
+++ final_project/GroupFinalAPI/index.js
@@ -28,9 +28,6 @@ const UsedCar = mongoose.model("UsedCar");
 require("./Models/newCars.js");
 const NewCar = mongoose.model("NewCar");
 
-require("./Models/customers.js");
-const Customer = mongoose.model("Customer");
-
 require("./Models/employees");
 const Employee = mongoose.model("Employee");
 
