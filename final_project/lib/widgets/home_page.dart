42d894545ed2624c7dad7f62f2d0c4635cd0fc0e
PaulRoss22 <95063507+PaulRoss22@users.noreply.github.com>
2022-12-15 21:38:09 -0600

    homepage image

--- final_project/lib/widgets/home_page.dart
+++ final_project/lib/widgets/home_page.dart
@@ -1,17 +1,25 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'app_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  
-      @override
-      Widget build(BuildContext context) {
-      return const Scaffold(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(75),
         child: AppBarLogo(),
-      )
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/vibes.png"),
+            fit: BoxFit.fill,
+          ),
+        ),
+      ),
     );
   }
 }
